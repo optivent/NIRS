be0520f000ff2960d89d777a69ccf029ca041fe0
I G <igeorgevici@outlook.com>
2020-02-20 15:13:26 +0100

    unclear changes

--- feature_importance.R
+++ feature_importance.R
@@ -3,314 +3,232 @@ library(tidyverse); library(zoo); library(bestNormalize)
 library(tidyr); library(feather); library(here)
 setwd(here("inputs"))
 
-stdize = function(x, ...) {(x - min(x, ...)) / (max(x, ...) - min(x, ...))} # min-max scaling
+
+
+stdize = function(x, ...) {(x - min(x, ...)) / (max(x, ...) - min(x, ...))}
 autonorm <- function(x, r, k) (bestNormalize(x, r, k, warn =F)$x.t)
 
-######### IMPORT DATA ########################################################################
+no_interpolation <- read_feather("manualtable.feather") %>% 
+  as_tibble() %>% 
+  filter(!is.na(NIRS_proc_min)) %>% dplyr::select(-c(NIRS_min, timediff)) 
 
-no_interpolation <- read_feather("manualtable.feather") %>% as_tibble() %>% 
-  filter(!is.na(NIRS_proc_min)) %>% dplyr::select(-c(NIRS_min))
+anae <- no_interpolation %>% filter(Group != "Sedation") %>% dplyr::select(-Group) %>% 
+  mutate_if(is.numeric, stdize, na.rm = TRUE) 
+DataExplorer::plot_histogram(anae)
   
-DataExplorer::plot_missing(no_interpolation %>% filter(Group != "Sedation"))   # missing anesthesie
-DataExplorer::plot_histogram(no_interpolation %>% filter(Group != "Sedation")) # histogram anesthesie
-DataExplorer::plot_missing(no_interpolation %>% filter(Group == "Sedation"))   # missing sedation
-DataExplorer::plot_histogram(no_interpolation %>% filter(Group == "Sedation")) # histogram sedation
-  
-######### DRAW REPRESENTATIVE SAMPLES ########################################################
+seda <- select(anti_join(no_interpolation, anae), -Group) %>% mutate_if(is.numeric, stdize, na.rm = TRUE)  
+DataExplorer::plot_histogram(seda)
 
+
+
+
+
+
+ 
 sensibility <- 2 # continous value between 1 and 3  # sensibility for the sampling
-library("tidyimpute")
-interpolation <- read_feather("manualtable.feather") %>%
-  filter(!is.na(NIRS_proc_min)) %>% 
+
+manualtable <- read_feather("manualtable.feather") %>% 
   group_by(Patient) %>%
    complete(
     timediff = seq(
       from = min(timediff),
-      to   = max(timediff))
-    ) %>% 
-    mutate_at(vars(Group:Weight), list(~zoo::na.locf(.,na.rm = TRUE))) %>% 
-    mutate_at(vars(SpO2:NIRS_min), list(~zoo::na.approx(., maxgap = 120, na.rm = FALSE))) %>% # i set the maxgap to 120 minutes to reduce the missing values
-    impute_at(.na=mean,  .vars=6:12, na.rm = TRUE) %>% 
+      to   = max(timediff))) %>% 
+    mutate_at(vars(Group:Weight),
+              list(~zoo::na.locf(.,na.rm = TRUE))) %>% 
+    mutate_at(vars(SpO2:NIRS_min),
+          list(~zoo::na.approx(., maxgap = 5, na.rm = FALSE))) %>% 
     mutate(
        diff_NIRSproc = replace_na(NIRS_proc_min - dplyr::lag(NIRS_proc_min),0),
        HF_proc = 100*(HF - first(HF))/first(HF),
        sampling_prob = replace_na(NIRS_proc_min^sensibility, 0)) %>% 
-  ungroup() %>% as_tibble() %>%
-  arrange(Patient, timediff) %>%
-  mutate(NIRS_deviation = as.factor(ifelse(NIRS_proc_min < 0, "negative deviation", "positive deviation"))) 
+    mutate_at(vars(sampling_prob), stdize, na.rm = T) %>% 
+  ungroup() %>% as_tibble() %>% 
+  filter(!is.na(NIRS_proc_min)) %>% 
+  mutate(NIRS_deviation = as.factor(ifelse(NIRS_proc_min < 0, "negative", "positive")))
 
+first_and_last <- manualtable %>%
+  group_by(Patient) %>%
+    arrange(timediff) %>% slice(c(1,n())) %>% 
+  ungroup() 
 
 set.seed(111)
-first_and_last <- group_by(interpolation, Patient) %>% slice(c(1,n())) %>% ungroup() 
-middle_samples <- group_by(interpolation, Patient) %>% sample_n(size = 8, replace = FALSE, weight = sampling_prob) %>% ungroup()
+middle_samples <- manualtable %>% 
+  group_by(Patient) %>% 
+    sample_n(size = 8, replace = FALSE, weight = sampling_prob) %>% 
+  ungroup()
+
 samples <- rbind(first_and_last, middle_samples) %>% arrange(Patient, timediff) %>%
-  select(-c(sampling_prob, NIRS_min)) 
-sample_anae <- samples %>% filter(Group != "Sedation")
+   select(-c(timediff, NIRS_min, diff_NIRSproc, sampling_prob))
+
+rm(first_and_last, middle_samples)
+
+#manualtable %>% group_by(Group, NIRS_deviation) %>% count()
+
+sample_anae_scaled <- samples %>% filter(Group != "Sedation") %>% 
+  dplyr::select(-NIRS_deviation) %>% 
+  mutate_if(is.numeric, autonorm, r = 10, k = 10)
+
 sample_seda <- samples %>% filter(Group == "Sedation")
-  
-rm(first_and_last, middle_samples, samples, sensibility)
-DataExplorer::plot_missing(sample_anae)
-DataExplorer::plot_missing(sample_seda)
 
-############ CORRELATION MATRICES ############################################################
-library(corrplot)
+# library(corrplot)
+# # 
+# cor_matrix <- cor.mtest(sample_anae, conf.level = .95)
+# # 
+# corrplot(cor_matrix, p.mat = cor_matrix$p, sig.level = .2,
+#          type = "lower",  method = "number",
+#          na.label = "●",
+#          tl.col = "black",tl.srt = 45,
+#          order = "hclust")
 
-sample_anae_cor <- sample_anae %>% select(-c(diff_NIRSproc)) %>% select_if(is.numeric) %>% cor()
-diag(sample_anae_cor) <- NA
-res1 <- cor.mtest(sample_anae %>% select_if(is.numeric), conf.level = .95)
+# library(Boruta)
+# set.seed(111)
+# plot(Boruta(NIRS_deviation ~ ., 
+#             data = na.omit(sample_anae ), 
+#             doTrace = 2), cex.axis=.7, las=2, xlab="", main="Variable Importance")
+# 
+# plot(Boruta(NIRS_deviation ~ ., 
+#             data = sample_seda %>% select(-c(CO2, BP_sys:BP_mid)), doTrace = 2),
+#      cex.axis=.7, las=2, xlab="", main="Variable Importance")
+############################### MUVR ######################
 
-sample_seda_cor <- sample_seda %>% select(-c(diff_NIRSproc, FiO2)) %>% select_if(is.numeric) %>% cor()
-diag(sample_seda_cor ) <- NA
-res2 <- cor.mtest(sample_seda %>% select_if(is.numeric), conf.level = .95)
-
-col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
-
-par(mfrow=c(1,1))
-
-corrplot(sample_anae_cor, 
-         type = "lower", order = "hclust", method = "color", 
-         p.mat = res1$p, sig.level = 0.05, insig = "blank",
-         diag = FALSE, tl.col = "black",tl.srt = 45,addCoef.col = "black",
-         title = "Correlation Matrix in Anesthesia", mar=c(0,0,1,0))
-
-corrplot(sample_seda_cor, 
-         type = "lower", order = "hclust", method = "color", 
-         p.mat = res1$p, sig.level = 0.05, insig = "blank",
-         diag = FALSE, tl.col = "black",tl.srt = 45,addCoef.col = "black",
-         title = "Correlation Matrix in Sedation", mar=c(0,0,1,0))
-
-
-rm(sample_anae_cor, sample_seda_cor, res1, res2, col)
-
-
-#### MUVR multilevel feature selection only the PLS, no multilevel in RF #####################
-library(doParallel); nCore=detectCores()-1   # Number of processor threads to use
-library(MUVR)
-
-# no_interpolation_anae <- no_interpolation %>% filter(Group != "Sedation") %>% na.omit() %>% select(-timediff)
-# no_interpolation_seda <- no_interpolation %>% filter(Group == "Sedation") %>% select(-c(CO2,timediff)) %>% na.omit()
-
-nRep=nCore*10            # Number of MUVR repetitions
-nOuter=7                # Number of outer cross-validation segments
-varRatio=0.9            # Proportion of variables kept per iteration 
-method='PLS'            # Selected core modelling algorithm
-
-
-## start cluster
-cl=makeCluster(nCore); registerDoParallel(cl)
-
-  # regrModel.anae = MUVR(X=no_interpolation_anae  %>% select(Months:CO2) %>% as.matrix(),
-  #                Y=no_interpolation_anae  %>% pull(NIRS_proc_min) %>% as.vector(),
-  #                ID= no_interpolation_anae $Patient,
-  #                nRep=nRep, nOuter=nOuter, varRatio=varRatio, method=method)
-  
-MUVR_sample_anae <- sample_anae %>% mutate_if(is.numeric, autonorm, r = 20, k = 5) 
-regrModel.anae.sample <-MUVR(
-    X= MUVR_sample_anae  %>% select(Months:CO2) %>% as.matrix(),
-    Y=MUVR_sample_anae   %>% pull(NIRS_proc_min) %>% as.vector(),
-    ID= MUVR_sample_anae$Patient,
-    nRep=nRep, nOuter=nOuter, varRatio=varRatio, method=method)
-  
-  # regrModel.seda = MUVR(X=no_interpolation_seda  %>% select(Months:BP_mid) %>% as.matrix(),
-  #                       Y=no_interpolation_seda %>% pull(NIRS_proc_min) %>% as.vector(),
-  #                       ID= no_interpolation_seda$Patient,
-  #                       nRep=nRep, nOuter=nOuter, varRatio=varRatio, method=method)
-
-MUVR_sample_seda <- sample_seda %>% mutate_at(vars(Months:BP_mid, NIRS_proc_min), autonorm, r = 20, k = 5)
-regrModel.seda.sample <-MUVR(
-    X= MUVR_sample_seda %>% select(Months:BP_mid) %>% as.matrix(),
-    Y= MUVR_sample_seda %>% pull(NIRS_proc_min) %>% as.vector(),
-    ID= MUVR_sample_seda$Patient,
-    nRep=nRep, nOuter=nOuter, varRatio=varRatio, method=method)
-  
-stopCluster(cl)
-## end cluster 
-
-
-regrModel.anae.sample$fitMetric
-regrModel.seda.sample$fitMetric
-
-#plotVAL(regrModel.anae)
-#plotMV(regrModel.anae, model='max')
-#plotStability(regrModel.anae, model='min')
-par(mfrow=c(1,2))
-# plotVIP(regrModel.anae, model='min')
-# plotVIP(regrModel.anae.sample, model='min')
-# plotVIP(regrModel.anae, model='med')
-plotVIP(regrModel.anae.sample, model='min')
-
-# plotVIP(regrModel.seda, model='min')
-# plotVIP(regrModel.seda.sample, model='min')
-# plotVIP(regrModel.seda, model='med')
-plotVIP(regrModel.seda.sample, model='med')
-
-par(mfrow=c(1,1)); 
-rm(cl, method, nCore, nOuter, nRep, varRatio, MUVR_sample_anae, MUVR_sample_seda)
-rm(regrModel.anae.sample, regrModel.seda.sample)
-
-#### TREE MODELS PRIMARILY FOR THE CHILDREN UNDER ANESTHESIA #####################
 
 library(rpart); library(rpart.plot)
+# sample_tree <- rpart(NIRS_deviation ~ ., control = list(maxdepth = 4), data = samples)
+# plot(sample_tree); text(sample_tree)
+# prp(sample_tree)
 
-sample_anae_tree <- rpart(NIRS_deviation ~ FiO2 + HF + SpO2,
-                          control = list(maxdepth = 5),
-                          data = sample_anae %>% filter(FiO2 > 22, Weight > 6), 
+sample_anae_tree <- rpart(NIRS_deviation ~ ., control = list(maxdepth = 3),
+                          data = sample_anae %>% dplyr::select(-HF_proc), 
                           model = TRUE)
-#plot(sample_anae_tree); text(sample_anae_tree)
-prp(sample_anae_tree); par(mfrow=c(1,1))
+plot(sample_anae_tree); text(sample_anae_tree)
+prp(sample_anae_tree)
 
-sample_seda_tree <- rpart(NIRS_deviation ~ FiO2 + HF + SpO2,
-                          control = list(maxdepth = 3),
-                          data = sample_seda)
-#plot(sample_seda_tree); text(sample_seda_tree)
-prp(sample_seda_tree); par(mfrow=c(1,1))
+table(predict(sample_anae_tree, type="class"), sample_anae$NIRS_deviation)
 
 
-sample <- rbind(sample_anae, sample_seda)
-sample_tree <- rpart(NIRS_deviation ~ Group + Weight + SpO2 + HF + BP_sys + BP_dia + BP_mid,
-                          control = list(maxdepth = 3),
-                          data = sample)
-plot(sample_tree); text(sample_tree)
-prp(sample_tree)
+
+sample_seda_tree <- rpart(NIRS_deviation ~ ., control = list(maxdepth = 3), data = sample_seda)
+plot(sample_seda_tree); text(sample_seda_tree)
+prp(sample_seda_tree)
 
 
 library(C50)
-c5tree <- C5.0(NIRS_deviation ~ Group + Weight + SpO2 + HF + BP_dia, 
-               data = sample, 
-               trials = 5,
+c5tree <- C5.0(NIRS_deviation ~ ., 
+               data = sample_anae %>% dplyr::select(-c(HF_proc,Group)), 
+               trials = 50,
                control = C5.0Control(winnow = TRUE))
 summary(c5tree)
 C5imp(c5tree)
 plot(c5tree)
 
-rm(c5tree, sample_anae_tree, sample_seda_tree)
 
-#### MULTILEVEL BAYESIAN MODEL ###################################################
 
-scaled_no_interpolation <- no_interpolation %>%
-  select(Patient, Group:NIRS_proc_min) %>% 
-  mutate_at(vars(Months:CO2), stdize, na.rm = T) 
-  
+# next feature importance based on multilevel bayesian models
+
+scaled_table <- manualtable %>% select(Patient, Group:NIRS_proc_min) %>% 
+  mutate_at(vars(Months:CO2), stdize, na.rm = T)
+
+anae_scal <- scaled_table %>% filter(Group != "Sedation") %>% select(-Group)
+seda_scal <- scaled_table %>% filter(Group == "Sedation") %>% select(-Group)
+rm(scaled_table)
+
 library(parallel)
 library(doParallel)
 library(rstanarm)
-options(mc.cores = parallel::detectCores()-2)
+options(mc.cores = parallel::detectCores()-1)
 
-bayes_HLM <- stan_glmer(NIRS_proc_min ~ FiO2 + HF + (1|Patient),
-                        family="gaussian",
-                        data= no_interpolation,
-                        iter = nrow(scaled_no_interpolation)*5, 
-                        chains = parallel::detectCores()-2)
+bayes_HLM <- stan_glmer(NIRS_proc_min ~ FiO2 + SpO2 + Weight + HF  + BP_dia + CO2 + 
+                          (1|Patient),
+                        data=anae_scal, family="gaussian", iter = 4250, chains = 7)
 
 library(report)
 bayes_HLM %>% report() %>% to_fulltable()
 bayes_HLM %>% report(standardize="smart", effsize="cohen1988") %>% to_fulltext()
 
-
-#### NON-Linear Model ############################################################
-
-library("cgam") # constrained general additive models
-
-ans <- ShapeSelect(NIRS_proc_min ~ shapes(FiO2, set = c("incr")) +
-                     shapes(HF, set= c("incr")),
-                   data = sample_anae %>% filter(HF < 160))
-
-
-
-
-fit1 <- cgam(NIRS_proc_min ~ s.incr.incr(FiO2, HF, numknots = c(20, 20)), # manual generated
-             data = sample_anae %>% filter(HF < 150))
-plotpersp(fit1,  main = "3D Plot of a Smooth Cgam Fit1"); summary(fit1)
-
-autofit_anae_FIO2_HF <- ShapeSelect(NIRS_proc_min ~ shapes(SpO2) + shapes(HF),     # computer generated 
-                        data = sample_anae %>% filter(HF < 140), genetic = TRUE)
-
-
-autofit_seda_SpO2_HF <- ShapeSelect(NIRS_proc_min ~ shapes(SpO2) + shapes(HF),     # computer generated 
-                                    data = sample_seda %>% filter(HF < 140) %>% 
-                                      select(SpO2, HF, NIRS_proc_min) %>% na.omit(),
-                                    genetic = TRUE)
-
-
-
-sample_anae$HF %>% DataExplorer::plot_histogram()
-sample_anae$FiO2 %>% DataExplorer::plot_histogram()
-
-#### ITSADUG #####################################################################
-
 library(itsadug); library(mgcv)
-# mod_gam <- mgcv::gam(NIRS_proc_min ~ s(HF) + s(FiO2) + te(Patient, timediff, bs = 're'),
-#                      #correlation = corAR1(form = ~ time),
-#                      data = anae_scal,
-#                      method = "ML") 
-# # method = "GCV.Cp"  te(ID, time, bs="fs", m=1, k = 5),
-# # s(time) + s(ID, bs = 're'),
-# summary(mod_gam)
-# par(mfrow=c(2,2)); gam.check(mod_gam); par(mfrow=c(1,1))
-# 
-# fvisgam(mod_gam, view=c("HF", "FiO2"), rm.ranef=TRUE, main="fvisgam", dec=1)
+mod_gam <- mgcv::gam(NIRS_proc_min ~ s(HF) + s(FiO2) + te(Patient, timediff, bs = 're'),
+                     #correlation = corAR1(form = ~ time),
+                     data = anae_scal,
+                     method = "ML") 
+# method = "GCV.Cp"  te(ID, time, bs="fs", m=1, k = 5),
+# s(time) + s(ID, bs = 're'),
+summary(mod_gam)
+par(mfrow=c(2,2)); gam.check(mod_gam); par(mfrow=c(1,1))
 
+fvisgam(mod_gam, view=c("HF", "FiO2"), rm.ranef=TRUE, main="fvisgam", dec=1)
 
-# mod_gam_anae_HF_SPO2 <- mgcv::gam(NIRS_proc_min ~ te(HF, SpO2) +
-#                         s(Patient, bs = "re"),
-#                         #correlation = corAR1(form = ~ timediff),
-#                         data = sample_anae %>% 
-#                         filter(HF < 150, FiO2 < 90, HF > 70),
-#                       method = "ML") 
-# fvisgam(mod_gam_anae_HF_SPO2, view=c("HF", "SpO2"), rm.ranef=TRUE, main="fvisgam", dec=1)
-# summary(mod_gam_anae_HF_SPO2)
-# 
-# mod_gam_seda_HF_SPO2 <- mgcv::gam(NIRS_proc_min ~ s(HF, k = 5) + s(SpO2, k = 5) +
-#                                     s(Patient, bs = "re"),
-#                                   #correlation = corAR1(form = ~ timediff),
-#                                   data = no_ %>% 
-#                                     filter(HF < 150),
-#                                   method = "ML") 
-# fvisgam(mod_gam_seda_HF_SPO2, view=c("HF", "SpO2"), rm.ranef=TRUE, main="fvisgam", dec=1)
-# summary(mod_gam_seda_HF_SPO2)
-
-##
-
-# mod_gam_anae1 <- mgcv::gam(NIRS_proc_min ~ s(HF) + s(FiO2) + 
-#                         te(Patient, timediff, bs="fs", m=1, k = 5),
-#                      correlation = corAR1(form = ~ timediff),
-#                      data = no_interpolation %>% 
-#                        select(Patient, Group, timediff, HF, FiO2, NIRS_proc_min) %>% 
-#                        na.omit() %>% filter(Group != "Sedation", HF < 140, FiO2 < 80),
-#                      method = "ML") 
-# summary(mod_gam_anae1)
-# fvisgam(mod_gam_anae1, view=c("FiO2","HF"),
-#         rm.ranef=FALSE, main="anaesthesia", dec=1)
-
-
-mod_gam_anae2 <- mgcv::gam(NIRS_proc_min ~ te(FiO2,HF, bs="gp") + 
-                             t2(Patient, timediff, bs="fs"),
-                           correlation = corAR1(form = ~ timediff),
-                           data = no_interpolation %>% 
-                             select(Patient, Group, timediff, HF, FiO2, NIRS_proc_min) %>% 
-                             na.omit() %>% filter(Group != "Sedation", HF < 140, FiO2 < 80),
-                           method = "ML") 
-summary(mod_gam_anae2)
-
-par(mfrow=c(1,1))
-
-fvisgam(mod_gam_anae2, view=c("FiO2","HF"), rm.ranef=FALSE, main="anaesthesia", dec=0,
-        #plot.type = "persp",
-        color = "topo",
-        labcex = 1,
-        hide.label = TRUE
-        #nCol = 19
-        )
-
-
-plot(mod_gam_anae2, select=1, labcex = 0.7,  rug=FALSE)
+#################
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(lme4, glmmLasso, glmnet, mcmc, statmod, cluster,
+               factoextra, randomForest, party, nnet, mclust, MASS,
+               invgamma, coda, corrplot, lmmen, caret, xtable, bestNormalize)
 
 
 
+library(grpreg)
+library("lars")
+data(diabetes)
+attach(diabetes)
+
+# x1-x4: age, sex, BMI, BP;
+# x5-x10: serum measurements
+
+group <- c(rep(1,4), rep(2,6))
+par(mfrow=c(2,3))
+
+fit <- grpreg(x,y,group,penalty="grLasso") #will have some problems
+
+x = as.data.frame(x)
+x = as.matrix(x)
+fit <- grpreg(x,y,group,penalty="grLasso")
+
+plot(fit,main = "Group Lasso")
+
+fit <- grpreg(x,y,group,penalty="grMCP") # The former involves an MCP penalty being applied to an L2-norm of each group.
+plot(fit, main = "Group MCP")
+
+fit <- grpreg(x,y,group,penalty="grSCAD")
+plot(fit, main = "Group SCAD")
+
+# bi-level selection
+# Group exponential lasso
+
+#Bi-level means carrying out variable selection at the group level as well as the level of individual covariates (i.e., selecting important groups as well as important members of those groups)
+#Group selection selects important groups, and not members within the group â€“ i.e., within a group, coefficients will either all be zero or all nonzero.
+
+fit <- grpreg(x,y,group,penalty="gel") #Group exponential lasso
+plot(fit, main ="gel")
+
+fit <- grpreg(x,y,group,penalty="cMCP") # a hierarchical penalty which places an outer MCP penalty on a sum of inner MCP penalties for each group
+plot(fit, main ="cMCP")
+
+dev.off()
+
+#However, especially when p is large compared with n, grpreg may fail to converge at low values of lambda, where models are nonidentifiable or nearly singular. Often, this is not the region of the coefficient path that is most interesting.
+
+res <- select(fit, criterion = "AIC")
+res$lambda
+
+# cross-validation
+# default penalty is grLasso
+
+par(mfrow=c(1,2))
+
+cvfit <- cv.grpreg(x, y, group, seed =12345)
+
+# lambda based on minimum cv error rule
+cvfit$lambda.min
+cvfit$cve
+cvfit$cvse
+
+plot(cvfit)
+summary(cvfit)
+coef(cvfit) ## Beta at minimum CVE
+
+cvfit <- cv.grpreg(x, y, group, penalty = "grSCAD")
+plot(cvfit)
+summary(cvfit)
+coef(cvfit) ## Beta at minimum CVE
 
 
-
-##
-
-
-
+dev.off()
